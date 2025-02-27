af5512165775ceb4ace31433118331d4be82cb94
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-02-27 19:20:47 +0530

    addred react router and threw error due to tailwind so reinstalled tailwind and npm packages

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@
 
 // export default App
 
-
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom"
 import React from 'react'
 import Navbar from './components/Navbar.'
 import Hero from './components/Hero'
@@ -44,20 +44,23 @@ import HomeCards from "./components/HomeCards"
 import JobList from './components/JobList'
 import ViewAllJobs from './components/ViewAllJobs'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1> My App</h1>}/>)
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar/>
-      {/* <Hero title="Become a kotlin dev" subtitle="kotlin dev opp"/> pass the
-      values here for these to show up or let it remain empty so that default values can go in */}
-      <Hero/>
-      <HomeCards/>
-      <JobList/>
-      <ViewAllJobs/>
+  return <RouterProvider router={router}/>
+
+    // <>
+    //   <Navbar/>
+    //   {/* <Hero title="Become a kotlin dev" subtitle="kotlin dev opp"/> pass the
+    //   values here for these to show up or let it remain empty so that default values can go in */}
+    //   <Hero/>
+    //   <HomeCards/>
+    //   <JobList/>
+    //   <ViewAllJobs/>
       
-    </>
-  )
+    // </>
 }
 
 export default App
