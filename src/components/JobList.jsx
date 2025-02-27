ce88d2197be79b5f02c164b29c045759fb2953dd
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-02-27 21:12:39 +0530

    fetched data from backend(json-server) used useEffect and useState

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import jobs from "../jobs.json"
 import JobCard from './JobCard'
+import { useEffect, useState } from 'react'
 
 const JobList = ({isHome = false}) => {
     
-      const jobListings = isHome? jobs.slice(0,3): jobs
+    const [job, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => { 
+      const fetchJobs = async () => {
+        try{
+          const res = await fetch("http://localhost:5000/jobs")
+          const data = await res.json();
+          setJobs(data)
+        }catch(error){
+          console.log("error fetching data"+ error);
+        }finally{
+          setLoading(false);
+        }
+
+      }
+
+      fetchJobs();
+    }, []);
+
+    const jobListings = isHome ? job.slice(0,3): job
   
     return (
       <>
@@ -14,9 +34,18 @@ const JobList = ({isHome = false}) => {
             {isHome? "Recent Jobs" : "Browse Jobs"}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {jobListings.map((job) => (
-                <JobCard key={job.id} job={job}/>
-              ))}
+              {loading? 
+                (
+                <h2>Loading..</h2>
+                ):  (
+                  <>
+                    {jobListings.map((job) => (
+                      <JobCard key={job.id} job={job}/>
+                    ))}
+                  </>
+                ) 
+              }
+              
           </div>
         </div>
       </section>
