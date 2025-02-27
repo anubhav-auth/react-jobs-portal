85a6b6c75b0d6ebbf3cbce669bacc7436b01f81e
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-02-27 20:00:01 +0530

    replaced a;; <a> with <Link> from react-router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from "../assets/images/logo.png"
 const Navbar = () => {
   return (
@@ -21,20 +22,20 @@ const Navbar = () => {
             </a>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/"
+                <Link
+                  to="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
+                  >Home</Link
                 >
-                <a
-                  href="/jobs"
+                <Link
+                  to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
+                  >Jobs</Link
                 >
-                <a
-                  href="/add-job"
+                <Link
+                  to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
+                  >Add Job</Link
                 >
               </div>
             </div>
