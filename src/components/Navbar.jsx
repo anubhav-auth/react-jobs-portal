7af08dfb2208171a80e1bd9b9938b126cbe77edb
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-03-03 22:51:18 +0530

    completed single job page to show job details when a job card is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import logo from "../assets/images/logo.png"
 const Navbar = () => {
   const linkClass = ({isActive})=> isActive? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
@@ -11,7 +11,7 @@ const Navbar = () => {
           <div
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" href="/">
               <img
                 className="h-10 w-auto"
                 src={logo}
@@ -20,7 +20,7 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </a>
+            </Link>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <NavLink
